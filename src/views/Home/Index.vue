22b987f9433c905c7665175eee2a3f88ca8e134c
Janile Dantas <janile@meucredere.com.br>
2021-08-13 01:59:33 -0300

    Ícone book / messageBookEmpty - Index

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,11 +1,33 @@
 <template>
+  <section class="home-index-content">
+    <img src="@/assets/images/illustrations/ic-book.svg" class="ic_book">
+
+    <p>{{ messageBookEmpty }}</p>
+      
+  </section>
 </template>
 
 <script>
   export default {
     name: 'HomeIndex',
+    data() {
+      return {
+        messageBookEmpty: 'Nenhum contato foi criado ainda.'
+      }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
+  @import '@/assets/stylesheets/variables/_sizes.scss';
+
+  .home-index-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .ic_book {
+    margin: 0 0 3*$x ;
+  }
 </style>
