13b927dc2ec4ce2496c2db64ab24f1c6fe0d3667
Janile Dantas <janile@meucredere.com.br>
2021-08-12 22:18:21 -0300

    Helpers - form

---
+++ src/_CRUDL/helpers/form/fields.js
@@ -0,0 +1,16 @@
+export const input = {
+  callback: null,
+  data: {},
+  editing: false,
+  errors: [],
+  mask: {},
+  processing: false,
+  skip: false,
+  type: 'input',
+  validated: null,
+  validable: false,
+  validations: [{
+    type: 'blank',
+  }],
+  value: undefined,
+};
