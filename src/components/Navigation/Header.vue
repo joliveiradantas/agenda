e703336a35a8769d875f1f895c1a4ab7af206140
Janile Dantas <janile@meucredere.com.br>
2021-08-17 02:54:54 -0300

    Aplicando botão "Criar contato" no header

--- src/components/Navigation/Header.vue
+++ src/components/Navigation/Header.vue
@@ -2,17 +2,40 @@
   <header class="bg_header">
     <img src="@/assets/images/illustrations/ic-logo.svg" class="ic_logo">
 
+    <former-button
+      v-if="buttonActive"
+      class="button"
+      icon="plus" 
+      :label="buttonLabel"
+      :buttonType="buttonType"
+    >      
+    </former-button>
+
     <search-bar @input="input" />
   </header>
 </template>
 
 <script>
   import SearchBar from '@/components/Search/Bar.vue';
+  import FormerButton from '@/components/Former/Button.vue';
 
   export default {
     name: 'NavigationHeader',
     components: {
-      SearchBar
+      SearchBar,
+      FormerButton,
+    },
+    data() {
+      return {
+        buttonLabel: 'Criar contato',
+        buttonType: 'creationButton',
+      }
+    },
+
+    computed: {
+      buttonActive() {
+        return this.$route.name === 'contacts-list';
+      }
     },
 
     methods: {
@@ -28,16 +51,20 @@
 
   .bg_header {
     display: flex;
-
     height: 8*$x;
+    justify-content: space-between;
     padding: 2*$x;
     width: 180*$x;
   }
 
   .ic_logo {
     height: 4*$x;
-    margin: 0 28.5*$x 0 0 ;
+    margin-right: 7.5*$x;
     object-fit: contain;
     width: 18.5*$x;
+  }
+
+  .button {
+    margin-right: 3*$x;
   }
 </style>
